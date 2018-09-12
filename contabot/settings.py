b5e97a8378ca407d4f318f6e5ccf7014ce4c204e
Henry Bravo <dev@backstartup.com>
2018-09-12 13:00:37 -0500

    Test

--- contabot/settings.py
+++ contabot/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'contabot',
         'USER': 'contabot',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PASSWORD': 'contabot',
         'PORT': 5432,
     }
